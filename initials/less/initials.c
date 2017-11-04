b900b287b0abe94b6051a49b0e263ef567d32c57
Israel Mesa <mesa.israel50@gmail.com>
2017-11-04 02:18:45 +0000

    initials

---
+++ initials/less/initials.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string name = get_string();
+    if(name != NULL)
+    {
+        for(int i = 0, length = strlen(name); i < length; i++)
+        {
+            if(i == 0)
+            {
+                printf("%c", toupper(name[i]));
+            }
+            else if(name[i] == 32)
+            {
+                printf("%c", toupper(name[i + 1]));
+            }
+        }
+        printf("\n");
+    }
+}
